e23190e18dd5f3af09bd8e88c251d2bac99b7a32
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2024-03-07 21:13:21 +0000

    create project structure

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_html():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+# TODO: add random number generation 
+# logic and pass using template
